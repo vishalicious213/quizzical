33416011867caf8c8ee97f396dcb188d8819f998
vishalicious213 <vishalicious213@gmail.com>
2023-07-20 16:01:57 -0700

    updated Splash with elements

--- components/Splash.jsx
+++ components/Splash.jsx
@@ -2,6 +2,10 @@ export default function Splash({screen, toggleScreen}) {
     console.log(screen)
 
     return (
-        <section className="hero">SPLASH SCREEN</section>
+        <section className="splash">
+            <h1>Quizzical</h1>
+            <div>Some description if needed</div>
+            <button>Start quiz</button>
+        </section>
     )
 }

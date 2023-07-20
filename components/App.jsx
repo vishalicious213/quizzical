3500558557b719dcc09e033efd776ac7908943db
vishalicious213 <vishalicious213@gmail.com>
2023-07-20 15:53:29 -0700

    added screen state to App. passing it to Splash & Quiz as props. will use it to set which screen to display

--- components/App.jsx
+++ components/App.jsx
@@ -1,11 +1,14 @@
+import { useState } from "react"
 import Splash from "./Splash"
 import Quiz from "./Quiz"
 
 export default function App() {
+    const [screen, setScreen] = useState("splash")
+
     return (
         <main>
-            <Splash />
-            <Quiz />
+            <Splash screen={screen} />
+            <Quiz screen={screen} />
         </main>
     )
 }

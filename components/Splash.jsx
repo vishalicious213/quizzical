8a94e31b99eb9f346678fc448efbd7f4f4026c07
vishalicious213 <vishalicious213@gmail.com>
2023-07-20 16:16:07 -0700

    Splash: styled h1 & div

--- components/Splash.jsx
+++ components/Splash.jsx
@@ -4,7 +4,7 @@ export default function Splash({screen, toggleScreen}) {
     return (
         <section className="splash">
             <h1>Quizzical</h1>
-            <div>Some description if needed</div>
+            <div className="splash-desc">Some description if needed</div>
             <button onClick={() => toggleScreen("quiz")}>Start quiz</button>
         </section>
     )

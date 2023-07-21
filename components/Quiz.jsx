f971af7cb2e9543b119064f9e5e4ea9d37500a0b
vishalicious213 <vishalicious213@gmail.com>
2023-07-21 13:29:23 -0700

    Quiz: rendering question titles

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -2,6 +2,14 @@ export default function Quiz({toggleScreen, questions}) {
     console.log(questions)
 
     return (
-        <section className="quiz">QUIZ</section>
+        <section className="quiz">
+            {
+                questions.map((question, index) => (
+                        <div key={index} className="question">
+                            <h2 className="question-title">{question.question}</h2>
+                        </div>
+                ))
+            }
+        </section>
     )
 }

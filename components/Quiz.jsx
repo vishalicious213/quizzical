8df1af0cf6b5b30e281ea372600d4d10b32ed226
vishalicious213 <vishalicious213@gmail.com>
2023-07-21 13:47:16 -0700

    Quiz: rendering question answer choices as buttons

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -4,11 +4,26 @@ export default function Quiz({toggleScreen, questions}) {
     return (
         <section className="quiz">
             {
-                questions.map((question, index) => (
+                questions.map((question, index) => {
+                    let answersArr = [
+                        ...question.incorrect_answers,
+                        question.correct_answer
+                    ]
+                    console.log(answersArr)
+
+                    return (
                         <div key={index} className="question">
                             <h2 className="question-title">{question.question}</h2>
+                            {
+                                answersArr.map((answer, index) => {
+                                    return (
+                                        <button key={index}>{answer}</button>
+                                    )
+                                })
+                            }
                         </div>
-                ))
+                    )
+                })
             }
         </section>
     )

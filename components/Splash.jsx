b7737c87c28feef5026c07c023e3a0575fdc65c3
vishalicious213 <vishalicious213@gmail.com>
2023-07-20 17:23:36 -0700

    Splash: styled difficulty radio buttons & fieldset

--- components/Splash.jsx
+++ components/Splash.jsx
@@ -22,40 +22,48 @@ export default function Splash({screen, toggleScreen}) {
             <h1>Quizzical</h1>
             <div className="splash-desc">Test your knowledge on a variety of fun and interesting topics</div>
             
-            <form>
-                <fieldset>
+            <form className="setup-form">
+                <fieldset className="difficulty">
                     <legend>Difficulty</legend>
                     
-                    <input 
-                        type="radio"
-                        id="easy"
-                        name="difficulty"
-                        value="easy"
-                        checked={formData.difficulty === "easy"}
-                        onChange={handleChange}
-                    />
-                    <label htmlFor="easy">Easy</label>
+                    <div className="radio-container">
+                        <input 
+                            className="radio"
+                            type="radio"
+                            id="easy"
+                            name="difficulty"
+                            value="easy"
+                            checked={formData.difficulty === "easy"}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="easy">Easy</label>
+                    </div>
 
-                    <input 
-                        type="radio"
-                        id="medium"
-                        name="difficulty"
-                        value="medium"
-                        checked={formData.difficulty === "medium"}
-                        onChange={handleChange}
-                    />
-                    <label htmlFor="medium">Medium</label>
+                    <div className="radio-container">
+                        <input 
+                            className="radio"
+                            type="radio"
+                            id="medium"
+                            name="difficulty"
+                            value="medium"
+                            checked={formData.difficulty === "medium"}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="medium">Medium</label>
+                    </div>
 
-                    <input 
-                        type="radio"
-                        id="hard"
-                        name="difficulty"
-                        value="hard"
-                        checked={formData.difficulty === "hard"}
-                        onChange={handleChange}
-                    />
-                    <label htmlFor="hard">Hard</label>
-
+                    <div className="radio-container">
+                        <input 
+                            className="radio"
+                            type="radio"
+                            id="hard"
+                            name="difficulty"
+                            value="hard"
+                            checked={formData.difficulty === "hard"}
+                            onChange={handleChange}
+                        />
+                        <label htmlFor="hard">Hard</label>
+                    </div>
                 </fieldset>
             </form>
             <button onClick={() => toggleScreen("quiz")}>Start quiz</button>

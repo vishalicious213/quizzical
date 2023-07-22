2e8ac88b53cc64ac093310ab0f0b003cefac7d8e
vishalicious213 <vishalicious213@gmail.com>
2023-07-21 17:09:35 -0700

    Quiz: added shuffleAnswers(). T/F questions get their answers reverse-sorted (so its always true then false). multiple-choice questions get their answers randomized

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -3,6 +3,21 @@ import { decode } from "html-entities"
 export default function Quiz({toggleScreen, questions}) {
     console.log(questions)
 
+    function shuffleAnswers(array) {
+        if (array.length === 2 ) {
+            array.sort()
+            array.reverse()
+            console.log(array)
+        }
+
+        if (array.length > 2) {
+            for (let i = array.length - 1; i > 0; i--) {
+                const j = Math.floor(Math.random() * (i + 1));
+                [array[i], array[j]] = [array[j], array[i]];
+            }
+        }
+    }
+
     return (
         <section className="quiz">
             {
@@ -11,6 +26,8 @@ export default function Quiz({toggleScreen, questions}) {
                         ...question.incorrect_answers,
                         question.correct_answer
                     ]
+
+                    shuffleAnswers(answersArr)
 
                     return (
                         <div key={index} className="question">

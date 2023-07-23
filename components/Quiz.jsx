65c3b2fab81af1d52ba3b46fb0c0fbbd70d17881
vishalicious213 <vishalicious213@gmail.com>
2023-07-23 14:05:46 -0700

    Quiz: cleaned up code. the right answers are being stored in state and logged by handleSubmit()

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -10,8 +10,6 @@ export default function Quiz({toggleScreen, questions}) {
         q4: ""
     })
 
-    // console.log(questions)
-
     function isBlank(item) {
         if (item === "") {
             return true
@@ -28,20 +26,13 @@ export default function Quiz({toggleScreen, questions}) {
             array.reverse()
         }
 
-        // let result = questionsFromState.every(isBlank)
-        // console.log(result)
-
         if (questionsFromState.every(isBlank)) {
-            console.log("state is empty")    
             if (array.length > 2) {
                 for (let i = array.length - 1; i > 0; i--) {
                     const j = Math.floor(Math.random() * (i + 1));
                     [array[i], array[j]] = [array[j], array[i]];
                 }
             }
-        } else {
-            console.log("state has data")
-            console.log(questionsFromState)
         }
     }
 
@@ -52,16 +43,13 @@ export default function Quiz({toggleScreen, questions}) {
 
     function handleChange(event, qIndex) {
         const {name, value} = event.target
-        // console.log(event.target)
-        // console.log(`q${qIndex}`, name, value)
+
         setFormData(prevFormData => {
             return {
                 ...prevFormData,
                 [name]: value
             }
         })
-
-        console.log("formData", formData)
     }
 
     return (
@@ -74,16 +62,13 @@ export default function Quiz({toggleScreen, questions}) {
                     ]
 
                     shuffleAnswers(answersArr)
-                    // console.log(qIndex, answersArr)
 
                     return (
                         <div key={qIndex} className="question">
                             <h2 className="question-title">{decode(question.question)}</h2>
                             {
                                 answersArr.map((answer, aIndex) => {
-                                    // console.log(`q${qIndex}`, `a${aIndex}`, answer)
                                     return (
-                                        // <button key={index} className="answer">{decode(answer)}</button>
                                         <div key={aIndex} className="answer">
                                             <input 
                                                 type="radio"

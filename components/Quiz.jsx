7f05ad49a986b6cbf29422fff2d94549fe75eca9
vishalicious213 <vishalicious213@gmail.com>
2023-08-08 16:24:42 -0700

    grabbing first question/answer/wrong answers and logging them

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -2,7 +2,16 @@ import { useState } from "react"
 import { decode } from "html-entities"
 
 export default function Quiz({toggleScreen, questions}) {
+    const [formData, setFormData] = useState([
+        {
+            q0: questions[0].question,
+            answer: questions[0].correct_answer,
+            wrong: questions[0].incorrect_answers
+        }
+
+    ])
     console.log(questions)
+    console.log(formData)
     return (
         <section>
             QUIZ

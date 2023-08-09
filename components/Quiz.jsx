0dbae5aa5278b15084384a6123776a5f4ac8393a
vishalicious213 <vishalicious213@gmail.com>
2023-08-09 11:37:03 -0700

    setting questionsArray to formData state in useEffect

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -2,20 +2,13 @@ import { useState, useEffect } from "react"
 import { decode } from "html-entities"
 
 export default function Quiz({toggleScreen, questions}) {
-    const [formData, setFormData] = useState([
-        {
-            q0: questions[0].question,
-            answer: questions[0].correct_answer,
-            wrong: questions[0].incorrect_answers
-        }
-    ])
+    const [formData, setFormData] = useState([])
 
     useEffect(() => {
         generateQuestions()
     }, [])
 
     function generateQuestions() {
-        // console.log(questions)
         let questionsArray = []
         
         questions.forEach(q => {
@@ -24,25 +17,15 @@ export default function Quiz({toggleScreen, questions}) {
                 answers: [...q.incorrect_answers, q.correct_answer],
                 correct: q.correct_answer
             }
-            // newQuestion.question = q.question
-            // newQuestion.answers = [...q.incorrect_answers, q.correct_answer]
-            
-            // console.log(q.question, q.correct_answer, q.incorrect_answers)
-            // console.log(newQuestion)
+
             questionsArray.push(newQuestion)
-        }
-            // questionsArray.push(
-            // q.question,
-            // q.correct_answer,
-            // q.incorrect_answers
-        )
+        })
 
-        console.log(questionsArray)
-
-        // questions.forEach(q => console.log(q.question))
+        // console.log(questionsArray)
+        setFormData(questionsArray)
     }
 
-    // console.log(formData)
+    console.log(formData)
     return (
         <section>
             QUIZ

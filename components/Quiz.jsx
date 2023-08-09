58e42bf576e36ec806f1b01b70b03396b0f0726d
vishalicious213 <vishalicious213@gmail.com>
2023-08-09 11:30:41 -0700

    newQuestion is holding the correct question and answers array

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -19,9 +19,12 @@ export default function Quiz({toggleScreen, questions}) {
         let questionsArray = []
         
         questions.forEach(q => {
-            let newQuestion = {}
-            newQuestion.question = q.question
-            newQuestion.answers = [...q.incorrect_answers, q.correct_answer]
+            let newQuestion = {
+                question: q.question,
+                answers: [...q.incorrect_answers, q.correct_answer]
+            }
+            // newQuestion.question = q.question
+            // newQuestion.answers = [...q.incorrect_answers, q.correct_answer]
             
             console.log(q.question, q.correct_answer, q.incorrect_answers)
             console.log(newQuestion)

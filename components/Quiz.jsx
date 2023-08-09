517d0a5d6bb42858b18920f27973224d09e99ba3
vishalicious213 <vishalicious213@gmail.com>
2023-08-09 11:33:33 -0700

    added 'correct' to newQuestion and pushed newQuestion(s) to questionsArray

--- components/Quiz.jsx
+++ components/Quiz.jsx
@@ -21,13 +21,15 @@ export default function Quiz({toggleScreen, questions}) {
         questions.forEach(q => {
             let newQuestion = {
                 question: q.question,
-                answers: [...q.incorrect_answers, q.correct_answer]
+                answers: [...q.incorrect_answers, q.correct_answer],
+                correct: q.correct_answer
             }
             // newQuestion.question = q.question
             // newQuestion.answers = [...q.incorrect_answers, q.correct_answer]
             
-            console.log(q.question, q.correct_answer, q.incorrect_answers)
-            console.log(newQuestion)
+            // console.log(q.question, q.correct_answer, q.incorrect_answers)
+            // console.log(newQuestion)
+            questionsArray.push(newQuestion)
         }
             // questionsArray.push(
             // q.question,
